fe13eb6ae48f54c06372d0fb722be89ccc3a7509
KRMeeag <kenthrazenmagbanua@gmail.com>
2026-05-07 19:52:32 +0800

    fix: adjust cors policies for FRONTEND_URL env var

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://hoppscotch.io",
+  origin: process.env.FRONTEND_URL || "https://hoppscotch.io",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
